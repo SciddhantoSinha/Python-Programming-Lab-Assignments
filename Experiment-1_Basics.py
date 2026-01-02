d36f527ad87f225599506113c30c88ef3a8a72b7
Sciddhanto Sinha <125180299+SciddhantoSinha@users.noreply.github.com>
2026-01-03 00:54:57 +0530

    Add files via upload

---
+++ Experiment-1_Basics.py
@@ -0,0 +1,10 @@
+print("\n :: Nursery Rhyme :: \n") 
+print("""Baa, baa, black sheep, Have you any wool?
+Yes sir, yes sir, Three bags full. One for the master, One for the dame,
+And one for the little boy
+Who lives down the lane \n\n""")
+a = "A"
+b = "B"
+c = "C"
+hello = "Hi, " + a + b + c
+print("Concatenation of the strings is :: ", hello)
