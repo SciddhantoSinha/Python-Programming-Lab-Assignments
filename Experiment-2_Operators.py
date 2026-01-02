97c051d467eab1557a885ab8f276dde964595d01
Sciddhanto Sinha <125180299+SciddhantoSinha@users.noreply.github.com>
2026-01-03 00:57:44 +0530

    Add files via upload

---
+++ Experiment-2_Operators.py
@@ -0,0 +1,17 @@
+num1=int(input("Enter 1st Number :: ")) 
+num2=int(input("Enter 2nd Number :: "))
+print("The 1st and 2nd Number you entered are :: ",num1, num2)
+sum = num1 + num2 
+sub = num1 - num2 
+div = num1 / num2
+fl_div = num1 // num2 
+multi = num1 * num2 
+expo = num1 ** num2 
+mod = num1 % num2
+print("Summation of Num1 and Num2 is :: ", sum) 
+print("Subtraction of Num1 and Num2 is :: ", sub) 
+print("Division of Num1 and Num2 is :: ", div) 
+print("Floor Division of Num1 and Num2 is :: ", fl_div) 
+print("Multiplication of Num1 and Num2 is :: ", multi) 
+print("Exponent of Num1 to power Num2 is :: ", expo)
+print("Remainder of division of Num1 and Num2 is :: ", mod)
